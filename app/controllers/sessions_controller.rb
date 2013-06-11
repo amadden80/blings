ec4fdf3990e97dcc3d47dc9c9a1127694d764b3b
amadden80 <amadden80@gmail.com>
2013-06-11 16:45:05 -0400

    Login When not found

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     puts 'located_user: ' + user.username
-    authenticated_user = user.authenticate params[:password]
-    if authenticated_user
-      session[:user_id] = authenticated_user.id
-      puts session
-      render text: 'You are authenticated', layout: true
+    if user.present?
+      authenticated_user = user.authenticate params[:password]
+      if authenticated_user
+        session[:user_id] = authenticated_user.id
+        puts session
+        render text: 'You are authenticated', layout: true
+      else
+        redirect_to sessions_new_path
+      end
     else
       redirect_to sessions_new_path
     end
